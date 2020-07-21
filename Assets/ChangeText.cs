f7fdd53222572a1e588a4d2afd92a11d0941fdbc
Vishal Pandey <vishalp055@gmail.com>
2020-07-21 16:06:22 +0530

    Session 6 - Audio and UI

---
+++ Assets/ChangeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+	public Text scoreText;
+	public Text userInput;
+
+	int i =20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Change()
+    {
+        scoreText.text = userInput.text.ToString();
+    }
+}
