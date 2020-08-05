1773aca0a113839a0029b36ce633401af7066b75
Vishal Pandey <vishalp055@gmail.com>
2020-08-05 13:40:37 +0530

    Update

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+
+    private void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
+
---
+++ Assets/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject prefab;
+    GameObject projectile;
+    private void FixedUpdate()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position + new Vector3(1,1,0);
+            projectile.GetComponent<Rigidbody>().velocity = Vector3.right * 20;
+            //RaycastHit hit;
+            //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit))
+            //{
+            //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.red);
+            //    Destroy(hit.collider.gameObject);
+            //}
+            //else
+            //{
+            //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 1000, Color.black);
+            //}
+        }
+        
+    }
+}
