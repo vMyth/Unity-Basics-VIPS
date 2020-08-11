868fdab34511aed14737d03ab19c91cdbdeaf68f
Vishal Pandey <vishalp055@gmail.com>
2020-08-11 22:52:39 +0530

    Animations

---
+++ Assets/ANIMATE.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANIMATE : MonoBehaviour
+{
+    public Animator anim;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            anim.SetTrigger("IDLE");
+
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("WALK");
+
+        if (Input.GetKeyDown(KeyCode.D))
+            anim.SetTrigger("DEATH");
+    }
+}
---
+++ Assets/TIMER.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TIMER : MonoBehaviour
+{
+    public Text timer;
+    public int m;
+    public int s;
+
+    private void Start()
+    {
+        m = 4;
+        s = 59;
+        Update();
+        StartCoroutine(timerRoutine());
+    }
+
+    private void Update()
+    {
+        if(s<10)
+            timer.text = "0" + m + ":" + "0" + s;
+        else
+            timer.text = "0" + m + ":" + s; 
+    }
+
+    IEnumerator timerRoutine()
+    {
+        while (m >= 0)
+        {
+            yield return new WaitForSeconds(1);
+            s = s - 1;
+            if (s < 0)
+            {
+                m = m - 1;
+                s = 59;
+            }
+        }
+        yield return null;
+    }
+}
